444f09a00947cfd1a844a0deafd5b5432dc3a585
diangana <39535013+diangana@users.noreply.github.com>
2021-04-04 21:58:02 +0200

    modification

--- connexion.php
+++ connexion.php
@@ -1,24 +1,47 @@
 <?php
 include("config.php");
+
 function verrifie_login($pseudo,$password){
+  $verif=false;
   global $conn;
   $sql = "SELECT * FROM utilisateurs WHERE pseudo='".$pseudo."'  AND password ='".$password."'";
   $req=$conn->query($sql);
-  return 1;
+  $req->execute();
+  if($result = $req->fetch(PDO::FETCH_ASSOC))
+    {
+      $verif=TRUE;
+  return $verif;
+}
+else {
+  $verif=false;
+  return $verif;
+}
 
 }
 $pseudo=$_POST['pseudo'];
 $password=$_POST['password'];
-if(isset($_POST['pseudo'],$_POST['password']))
-{
+$result=false;
+verrifie_login($pseudo,$password);
+if(verrifie_login($pseudo,$password)==true)
+  {
+    if (!session_id())
+    {
+      session_start();
+     $_SESSION['login'] = $pseudo;
 
-verrifie_login($_POST['pseudo'],$_POST['password']);
-  $dest="index.html";
-  echo $sql;
-  echo '<script language="JavaScript">window.location=\'' . $dest . '\'</script>';
-}
-else {
-  $dest="connexion.html";
-     echo '<script language="JavaScript">window.location=\'' . $dest . '\'</script>';
+     $message = 'Bonjour '.htmlspecialchars($_SESSION['login']).', vous êtes connecté';
+     $dest="index.html";
+     echo $message;
+      echo '<script language="JavaScript">window.location=\'' . $dest . '\'</script>';
+     exit;
+    }
+  }
+
+else
+{
+  echo "il faut remplir tout les champs";
+include('connexion.html');
+
+
 }
  ?>
--- inscription.js
+++ inscription.js
@@ -1,14 +1,19 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+window.addEventListener("DOMContentLoaded",function(){
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  let forms = document.querySelectorAll('.needs-validation');
+let nom=document.querySelectorAll('.text');
+let vide="";
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
+
+
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
@@ -18,3 +23,4 @@
       }, false)
     })
 })()
+}
