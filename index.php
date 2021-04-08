9fa7a397829dc5272a5a234ebc0e564ef9fc5a35
Rafael <tavaresrafael93@gmail.com>
2021-04-08 18:08:47 +0200

    debut page accueil

--- index.php
+++ index.php
@@ -2,29 +2,51 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
-    <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"-->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-<script scr="inscription.js"></script>
+    
     <title>YouMeal</title>
-  </head>
+    </head>
 
 <?php
 session_start();
 
 if(!isset($_SESSION['login']))
 {
-
   include('connexion.html');
   die('');
-
 }?>
+<div class="container">
+  <div class="row justify-content-center align-items-center mt-5">
+
+    <div class="col-3 text-center">
+      <img src="ressources/bouton_menu.png" alt="">
+    </div>
+
+    <div class="col-6 text-center">
+      <form  method="GET" action="">
+        <div class="barre_recherche_div">
+          <button class="barre_recherche_btn" type="submit" value="chercher"><img src="ressources/recherche.png" alt="recherche"></button>
+          <input class="barre_recherche_input" type="text" name="recherche">
+        </div>
+      </form>
+    </div>
+
+    <div class="col-3 text-center">
+      <img src="ressources/messagerie.png" alt="messages">
+    </div>
+
+  </div>
+</div>
+
 <p><a href="ajouter_recette.php">ajouter une recette</a></p>
-<form  method="GET" action="">
-  <input type="text" name="recherche" placeholder="recherche"><input type="submit" value="chercher">
-</form><?php
+
+<?php
 include('config.php');
 global $conn;
 if(isset($_GET['recherche']))
