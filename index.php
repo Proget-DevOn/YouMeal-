2d592daa3046a36645ad05b81ce6c70171c0017b
Rafael <tavaresrafael93@gmail.com>
2021-04-08 18:52:03 +0200

    avancement index.php

--- index.php
+++ index.php
@@ -11,95 +11,114 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
     
     <title>YouMeal</title>
-    </head>
+  </head>
+  <body>
+      
+    
+    <?php
+    session_start();
 
-<?php
-session_start();
+    if(!isset($_SESSION['login']))
+    {
+      include('connexion.html');
+      die('');
+    }?>
 
-if(!isset($_SESSION['login']))
-{
-  include('connexion.html');
-  die('');
-}?>
-<div class="container">
-  <div class="row justify-content-center align-items-center mt-5">
+    <!-- TOP -->
+    
+    <div class="container">
+      <div class="row justify-content-center align-items-center mt-5">
 
-    <div class="col-3 text-center">
-      <img src="ressources/bouton_menu.png" alt="">
-    </div>
-
-    <div class="col-6 text-center">
-      <form  method="GET" action="">
-        <div class="barre_recherche_div">
-          <button class="barre_recherche_btn" type="submit" value="chercher"><img src="ressources/recherche.png" alt="recherche"></button>
-          <input class="barre_recherche_input" type="text" name="recherche">
+        <div class="col-3 text-center">
+          <img src="ressources/bouton_menu.png" alt="">
         </div>
-      </form>
+
+        <div class="col-6 text-center">
+          <form  method="GET" action="">
+            <div class="barre_recherche_div">
+              <button class="barre_recherche_btn" type="submit" value="chercher"><img src="ressources/recherche.png" alt="recherche"></button>
+              <input class="barre_recherche_input" type="text" name="recherche">
+            </div>
+          </form>
+        </div>
+
+        <div class="col-3 text-center">
+          <img src="ressources/messagerie.png" alt="messages">
+        </div>
+
+      </div>
     </div>
 
-    <div class="col-3 text-center">
-      <img src="ressources/messagerie.png" alt="messages">
-    </div>
 
-  </div>
-</div>
 
-<p><a href="ajouter_recette.php">ajouter une recette</a></p>
-
-<?php
-include('config.php');
-global $conn;
-if(isset($_GET['recherche']))
-{
-  $sql="SELECT * from recettes WHERE
-  nom_recette LIKE '%".$_GET['recherche']."%'
-  OR auteur LIKE '%".$_GET['recherche']."%'
-  OR  regime like '%".$_GET['recherche']."%'
-  OR categorie like '%".$_GET['recherche']."%'";
-  $rep = $conn->query($sql);
-  while($donnees = $rep->fetch(PDO::FETCH_ASSOC))
-  {
+    <?php
+      include('config.php');
+      global $conn;
+      if(isset($_GET['recherche']))
+      {
+        $sql="SELECT * from recettes WHERE
+        nom_recette LIKE '%".$_GET['recherche']."%'
+        OR auteur LIKE '%".$_GET['recherche']."%'
+        OR  regime like '%".$_GET['recherche']."%'
+        OR categorie like '%".$_GET['recherche']."%'";
+        $rep = $conn->query($sql);
+        while($donnees = $rep->fetch(PDO::FETCH_ASSOC)){
     ?>
+
+
     <div class="row">
-  <div class="col-sm-2">
-    <h3><?php echo $donnees['nom_recette'];?></h3>
-    <a href="image/image_recettes/<?php  echo $donnees['image']; ?>"><img src="image/image_recettes/<?php  echo $donnees['image']; ?>" alt="" width="200" height="200"></a>
-    <p><a href="profil.php?pseudo=<?php  echo $donnees['auteur']; ?>">ajouter par <?php  echo $donnees['auteur'];?></a></p>
-    <p><?php echo $donnees['cout']; ?> <br/>Temps de realisation:<?php  echo $donnees['temps_execution']; ?></p>
-  </div>
-  <?php
- }
-?>
-</div>
-<?php
-}
-else {
-?>
-Bonjour
-<?php
-echo htmlspecialchars($_SESSION['login']);
-$sql='SELECT * FROM recettes';
-$rep = $conn->query($sql);
-while($donnees = $rep->fetch(PDO::FETCH_ASSOC))
-{
+      <div class="col-sm-2">
+        <h3><?php echo $donnees['nom_recette'];?></h3>
+        <a href="image/image_recettes/<?php  echo $donnees['image']; ?>"><img src="image/image_recettes/<?php  echo $donnees['image']; ?>" alt="" width="200" height="200"></a>
+        <p><a href="profil.php?pseudo=<?php  echo $donnees['auteur']; ?>">ajouter par <?php  echo $donnees['auteur'];?></a></p>
+        <p><?php echo $donnees['cout']; ?> <br/>Temps de realisation:<?php  echo $donnees['temps_execution']; ?></p>
+      </div>
 
-?>
 
-<div class="row">
-<div class="col-sm-2">
-<h3><?php echo $donnees['nom_recette'];?></h3>
-<a href="image/image_recettes/<?php  echo $donnees['image']; ?>"><img src="image/image_recettes/<?php  echo $donnees['image']; ?>" alt="" width="200" height="200"></a>
-<p><a href="profil.php?pseudo=<?php  echo $donnees['auteur']; ?>">ajouter par <?php  echo $donnees['auteur'];?></a></p>
-<p><?php echo $donnees['cout']; ?> <br/>Temps de realisation:<?php  echo $donnees['temps_execution']; ?></p>
-</div>
-<?php
-}
-?>
-</div>
-<?php
-}
+    <?php
+    }
+    ?>
 
-?>
-</div>
-</div>
+    </div>
+
+    <?php
+      }
+      else {
+      ?>
+      <h1 class="police_monogram mx-5 mt-5">Hello
+      <?php
+      echo htmlspecialchars($_SESSION['login']);?> !</h1>
+      <?php $sql='SELECT * FROM recettes';
+      $rep = $conn->query($sql);
+      while($donnees = $rep->fetch(PDO::FETCH_ASSOC))
+      {
+
+    ?>
+
+    <div class="row">
+      <div class="col-sm-2">
+        <h3><?php echo $donnees['nom_recette'];?></h3>
+        <a href="image/image_recettes/<?php  echo $donnees['image']; ?>"><img src="image/image_recettes/<?php  echo $donnees['image']; ?>" alt="" width="200" height="200"></a>
+        <p><a href="profil.php?pseudo=<?php  echo $donnees['auteur']; ?>">ajouter par <?php  echo $donnees['auteur'];?></a></p>
+        <p><?php echo $donnees['cout']; ?> <br/>Temps de realisation:<?php  echo $donnees['temps_execution']; ?></p>
+      </div>
+
+    <?php
+    }
+    ?>
+
+    </div>
+
+    <?php
+    }
+    ?>
+    </div>
+    </div>
+    <p class="mx-5 bolt"><strong>Qu'est-ce qu'on prépare aujourd'hui?</strong></p>
+
+    <div class="text-center mt-5"> 
+      <p><a href="ajouter_recette.php">ajouter une recette</a></p>
+    </div>
+
+  </body>
 </html>
