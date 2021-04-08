4ee3e4236f3c4c05ef848f510931921ce4934dce
diangana <39535013+diangana@users.noreply.github.com>
2021-04-08 18:11:28 +0200

    maj

--- ajout_recette.php
+++ ajout_recette.php
@@ -11,12 +11,17 @@ $auteur=$_POST['auteur'];
   $sql = "INSERT INTO recettes(nom_recette,date_recette,temps_execution,cout,note,auteur,categorie,regime,image) VALUES ('".$nom_recette."',NOW(),'".$temps_execution."','".$cout."',null,'".$auteur."','".$categorie."','".$regime."','".$_POST['image']."')";
   if(  $req=$conn->query($sql)==TRUE)
     {
-      $sql="SELECT id_recette FROM recettes WHERE nom_recette=$nom_recette and temps_execution=$temps_execution,cout=$cout,auteur=$auteur,categorie=$categorie,regime=$regime ";
-      $req=$conn->query($sql);
-      $donnees = $rep->fetch(PDO::FETCH_ASSOC);
-      $dest="ajout_ingredients.php?id_recette="+$donnes["id_recette"];
+      $sql2="SELECT * FROM recettes WHERE nom_recette='".$nom_recette."'  and  temps_execution='".$temps_execution."' and cout='".$cout."' and auteur='".$auteur."' and categorie='".$categorie."' and regime='".$regime."'";
+      $req2=$conn->query($sql2);
+      echo $sql2;
+      if ($donnees=$req2->fetch(PDO::FETCH_ASSOC)){
+        //echo $donnees["id_recette"];
+        $dest="ajout_ingredients.php?id_recette=".$donnees["id_recette"]."";
 
-       echo '<script language="JavaScript">window.location=\'' . $dest . '\'</script>';
+
+
+       echo '<script language="JavaScript">window.location=\'' . $dest.'\'</script>';
+     }
 }
 else{
   echo $sql;
