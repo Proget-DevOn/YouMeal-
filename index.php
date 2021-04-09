1a5d999f56a5423a97087af5f07ba42525d20f90
Rafael <tavaresrafael93@gmail.com>
2021-04-09 12:49:44 +0200

    barre de navigation

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
@@ -12,8 +16,31 @@
     
     <title>YouMeal</title>
   </head>
-  <body>
-      
+
+  <body class="fond_radiant">
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+          <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">YouMeal</label>
+
+      <div class="barre_recherche_div">
+        <form  method="GET" action="">
+          <button class="barre_recherche_btn" type="submit" value="chercher"><img src="ressources/recherche.png" alt="recherche"></button>
+          <input class="barre_recherche_input" type="text" name="recherche">
+        </form>
+      </div>
+
+      <ul>
+          <li><a href="#">MENU1</a></li>
+          <li><a href="#">MENU2</a></li>
+          <li><a href="#">MENU3</a></li>
+      </ul>
+
+      <a href=""><img src="ressources/messagerie.png" alt="messages"></a>
+
+    </nav>
     
     <?php
     session_start();
@@ -24,7 +51,9 @@
       die('');
     }?>
 
-    <!-- TOP -->
+    
+
+    <!-- Barre Navigation 
     
     <div class="container">
       <div class="row justify-content-center align-items-center mt-5">
@@ -49,6 +78,8 @@
       </div>
     </div>
 
+    -->
+    
 
 
     <?php
