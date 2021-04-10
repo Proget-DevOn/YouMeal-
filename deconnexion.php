4206bfdd5a097f7106884174e17dfe4f76eeb676
diangana <39535013+diangana@users.noreply.github.com>
2021-04-10 16:15:35 +0200

    d

--- deconnexion.php
+++ deconnexion.php
@@ -8,6 +8,6 @@ session_destroy();
 unset($_SESSION);
 unset($_COOKIE);
 header("Cache-Control: no-store, no-cache, must-revalidate");
-header('Location:index.html');
+header('Location:connexion.php');
 exit;
 ?>
