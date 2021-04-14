2dabef059209cf29e93ecf2e76c2a5d7d6b9f456
Raheriarisoa Eliott <eliott.raheriarisoa@gmail.com>
2021-04-14 21:11:25 +0200

    equipe

--- SiteWeb/equipe.php
+++ SiteWeb/equipe.php
@@ -1,10 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="fr-FR">
     <head>  
-        <title>Devon</title>
+        <title>Devon - Notre équipe</title>
         <meta charset="utf-8" />
-        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/equipe.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -19,50 +18,40 @@
 
         <div id="text-center">
             <div class="container police_cano">
-                <h1 id="h1">Bienvenue chez Devon !</h1>
-                <button onclick="scrollWin()" id="scroll">NOUS CONNAÎTRE</button>
-                <h2 id="scrolldown">Notre expertise</h2>
-                
-                <div id="images">
-                    <div class="image">
-                        <img id="image1" src="images/codage-web.png">
-                        <div>Développement web</div>
+                <div id="center">
+                    <div id="diana">
+                        <img id="diana-img" src="images/avatar-male.png" alt="">
+                        <div id="diana-description">Diana DIANGANA<br>Licence Pro DWM</div>
                     </div>
-                    <div class="image">
-                        <img id="image2" src="images/appel.png">
-                        <div>Développement d'applications</div>
-                    </div>
-                    <div class="image">
-                        <img id="image3" src="images/moniteur.png">
-                        <div>Hébergement web</div>
-                    </div>
-                    <div class="image">
-                        <img id="image4" src="images/mot-de-passe.png">
-                        <div>Sécurité informatique</div>
-                    </div>
-                    <div class="image">
-                        <img id="image5" src="images/reparer.png">
-                        <div>Maintenance</div>
-                    </div>
-                </div>
 
-                <h2>Ils nous font confiance</h2>
-                <div id="clients">
-                    <img id="client1" class="clients" src="images/airbnb.png" alt="">
-                    <img id="client2" class="clients" src="images/paypal.png" alt="">
-                    <img id="client3" class="clients" src="images/spotify.png" alt="">
-                    <img id="client4" class="clients" src="images/booking.png" alt="">
+                    <div id="cloe">
+                        <img id="cloe-img" src="images/avatar-male.png" alt="">
+                        <div id="cloe-description">Cloé SALEILLE<br>Licence Pro IW</div>
+                    </div>
+
+                    <div id="julie">
+                        <img id="julie-img" src="images/avatar-male.png" alt="">
+                        <div id="julie-description">Julie TANG<br>Licence Pro IW</div>
+                    </div>
+
+                    <div id="rafael">
+                        <img id="rafael-img" src="images/avatar-male.png" alt="">
+                        <div id="rafael-description">Rafael TAVARES DE PINO<br>Licence Pro DWM</div>
+                    </div>
+
+                    <div id="valentin">
+                        <img id="valentin-img" src="images/avatar-male.png" alt="">
+                        <div id="valentin-description">Valentin CELLES<br>Licence Pro DWM</div>
+                    </div>
+
+                    <div id="eliott">
+                        <img id="eliott-img" src="images/avatar-male.png" alt="">
+                        <div id="eliott-description">Eliott RAHERIARISOA<br>Licence Pro DWM</div>
+                    </div>
                 </div>
             </div>
         </div>
+        <?php include('footer.html');?>
+
     </body>
-    <?php include('footer.html');?>
-
 </html>
-
-<script>
-function scrollWin() {
-    document.getElementById('scrolldown').scrollIntoView();
-}
-</script>
-
