4849c2be4012331f3892caa080aede39cc61ca0c
Raheriarisoa Eliott <eliott.raheriarisoa@gmail.com>
2021-04-14 21:25:56 +0200

    apropos

---
+++ SiteWeb/a-propos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/a-propos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Oswald:wght@700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php include('header.html');?>
+    <div class="container police_cano">
+
+    <h1 id="h1">A propos</h1>
+    <div class="ligne_radiant_bot"></div>
+    <br>
+
+        <p>Bien plus qu’une agence web traditionnelle, Devon se distingue par sa forte spécialisation en développement Web et numérique.</p>
+        <p>Les valeurs de l’agence se portent essentiellement sur le localisme, en soutenant nos collaborateurs régionaux nous garantissons un service de qualité et un lien social important lors de tout échange.</p>
+            <p>Véritable vitrine d’un projet, le site ou l’application doit refléter ses valeurs et servir à forger son image de marque. </p>
+            <p>Il est donc particulièrement important de se démarquer de la concurrence par son unicité tant sur le plan du contenu que de la forme. </p>
+            <p>Nous concevons des sites e-commerce et créons des applications sur mesure pour vous de A à Z.</p>
+            <p>Pas d’inquiétude, avec Devon, vous ne serez pas déçu d’être venu ! Notre équipe s’engage à répondre au mieux aux demandes et assure l’hébergement ainsi que la maintenance des sites.</p>
+            <p>En proposant des images, des couleurs, une conception et des fonctionnalités authentiques, l’application doit convaincre sa cible et représenter au mieux ses intérêts. </p>
+            <p>Au fil du temps, la réputation numérique de chaque projet ou entreprise est à faire évoluer. En d’autres termes, pour rester le plus proche de la tendance, un projet est appelé à consacrer du temps à l’amélioration de son site/application. </p>
+            <p>C’est pour cela que nous accompagnons nos clients sur le long terme dans la stratégie numérique.</p>
+</div>
+        </body>
+</html>
+
+<?php include('footer.html');?>
