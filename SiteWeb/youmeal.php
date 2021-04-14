a241aead8c04fb445cd320cae400f84e978f3496
@CellesValentin <valentincelles@gmail.com>
2021-04-14 14:39:04 +0200

    Images + début

--- SiteWeb/youmeal.php
+++ SiteWeb/youmeal.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
     <head>  
-        <title>Gary's Shop - Qui sommes-nous ?</title>
+        <title>Projet YouMeal</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="css/youmeal.css" />
     </head>
     <body>
 
         <?php include('header.html');?>
-       
+
+        <h1>YouMeal</h1>
+
+        <p class="centre">YouMeal est un projet universitaire visant à créer une application de live de cuisine entre particuliers.</p>
+        <p class="centre">Organiser des diffusions en direct avec d'autres utilisateurs afin de cuisiner le même plat tous ensembles en visio !</p>
+        <img class="centre "src="YouMeal-\SiteWeb\css\image\LOGO-02 (2).png" alt="Logo YouMeal"/>
+        <img class="centre "src="YouMeal-\SiteWeb\css\image\Accueil.png" alt="Logo YouMeal"/>
+        <img class="centre "src="YouMeal-\SiteWeb\css\image\Page du live.png" alt="Logo YouMeal"/>
 
     </body>
 </html>
