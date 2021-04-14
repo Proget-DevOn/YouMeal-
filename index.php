978e5a3d53644e3f6b9e2e74ec5e9674c81f8549
diangana <39535013+diangana@users.noreply.github.com>
2021-04-14 18:36:23 +0200

    manifest

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['login']))
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#f216d4">
+    <meta name="theme-color" content="black">
 
 
     <link rel="apple-touch-icon" href="ressources/logo_youmeal.png">
@@ -25,8 +25,7 @@ if(!isset($_SESSION['login']))
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-    <link rel="manifest" id="manifest-placeholder">
-    <script src="sw.js" defer></script>
+    <link rel="manifest" href="/manifest.JSON" id="manifest-placeholder">
 
     <title>YouMeal</title>
   </head>
